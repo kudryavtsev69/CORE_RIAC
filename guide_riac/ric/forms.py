865b3922b55f9498afd9c1ba280ddf522a317788
kudryavtsev69 <kudryavtsev69@andex.ru>
2023-01-29 20:54:21 +0300

    RIC

---
+++ guide_riac/ric/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Radar
+
+
+class AddRadar(forms.ModelForm):
+    class Meta:
+        model = Radar
+        fields = ('title', 'body', 'image',)
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Название РЛС'}),
+            'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Описание РЛС'})
+        }
